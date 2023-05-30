ae989c46691d7c9721358a3e5cd17c373988d12b
Niranjan Rajkumar <128575600+Niranjanr0682@users.noreply.github.com>
2023-05-30 19:36:50 +0530

    TITLE EDIT

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,6 @@ self.addEventListener('install', (event) => {
           '/default.html',
           '/page.html',
           '/compress.html',
-          
           // Add more assets to cache
         ]);
       })
